74955b2ba98d27e63b7a66c68270c348042e08dd
Dale Corns <drcorns@hotmail.com>
2014-02-26 11:12:33 -0800

    ADD Input Box and Slider for Milage

--- View/src/gpview.js
+++ View/src/gpview.js
@@ -1,6 +1,6 @@
 $(function(){
 
-				// Run noUiSlider
+				// Run the range slider
 				$('#rslider').noUiSlider({
 					 range: [10,40]
 					,start: [20,30]
@@ -9,9 +9,26 @@ $(function(){
 						
 					}
 				});
+			//Run the milage slider
+			$('#mslider').noUiSlider({
+					 range: [10,40]
+					,start: [20,30]
+					,handles:1
+					,serialization:{
+						to: [$('#milesfromHwy')]
+						
+					}
+				});
+
+
+//Initialize range slider with values from brange and erange
+//This will be change to use the values returned from the server as
+//a result of the request for places along the way
 			
+$('#rgbtn').click(function(){
+	$(rslider).val([$('#brange').val(),$('#erange').val()]);
+});
 
 
-$(rslider).val(['10','40']);
 
 			});
